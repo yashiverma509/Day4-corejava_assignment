5d705c93beaf6b872257d11c1477581eb323018f
yashiverma509 <101495215+yashiverma509@users.noreply.github.com>
2022-03-21 11:41:07 +0300

    Create FrequencyMain.java

---
+++ Harman_Question1/FrequencyMain.java
@@ -0,0 +1,12 @@
+package Assignment4.ques1;
+
+public class FrequencytMain
+{
+   static PrintFrequency f;
+
+	public static void main(String[] args) {
+	
+        System.out.println("The frequency of each word in the file");		
+		f=new PrintFrequency();
+}
+}
